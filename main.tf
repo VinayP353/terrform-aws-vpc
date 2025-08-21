c7c05fd732b70834395ac651848f80da2c8b9ff7
Vinay <vinayreddypeddireddy@gmail.com>
2025-08-21 18:24:11 -0400

    feat(security): add web SG allowing TCP/80

--- main.tf
+++ main.tf
@@ -78,3 +78,22 @@ resource "aws_route_table_association" "private" {
   subnet_id      = each.value.id
   route_table_id = aws_route_table.private.id
 }
+
+resource "aws_security_group" "web" {
+  name        = "tf-web-sg"
+  description = "Allow HTTP"
+  vpc_id      = aws_vpc.this.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = { Name = "tf-web-sg" }
+}
